577e2e1ecbad81bf636a9cd7c0bada5805169fb1
valentinPavon <vpavonlopez02@gmail.com>
2023-05-12 10:43:26 +0200

    Cambio clases a plantas y zombies y añdao funcionalidad de movilidad de los zombies y estadisticas

--- clases.py
+++ clases.py
@@ -6,27 +6,39 @@ class Personaje:
     vida = 0
     daño = 0
 
+
+
+class Plantas(Personaje):
+        def __init__(self,nombre,vida,daño):
+            Personaje.__init__(self)
+            self.nombre = nombre
+            self.vida = vida
+            self.daño = daño
+
+        def golpearZombie(self,enemigo):
+            ataque = self.daño + random.randrange(-10,11)
+            enemigo.vida = enemigo.vida - ataque
+            print(enemigo.nombre)
+            print('ATACA',self.nombre)
+            print('Vida del enemigo:',enemigo.vida)
+
+        def explotarZombie(self,enemigo):
+            enemigo.vida = 0
+            print('ATACA',self.nombre)
+            print('Vida del enemigo:',enemigo.vida)
+
+        def mejoraDaño(self):
+            self.daño+=5
+            print('Daño de',self.nombre,'mejorado:',self.daño)
+
+
+
+class Zombies(Personaje):
     def __init__(self,nombre,vida,daño):
+        Personaje.__init__(self)
         self.nombre = nombre
         self.vida = vida
         self.daño = daño
-
-    def golpearZombie(self,enemigo):
-        ataque = self.daño + random.randrange(-10,11)
-        enemigo.vida = enemigo.vida - ataque
-        print(enemigo.nombre)
-        print('ATACA',self.nombre)
-        print('Vida del enemigo:',enemigo.vida)
-
-    def explotarZombie(self,enemigo):
-        enemigo.vida = 0
-        print('ATACA',self.nombre)
-        print('Vida del enemigo:',enemigo.vida)
-
-    def mejoraDaño(self):
-        self.daño+=15
-        print('Daño de',self.nombre,':',self.daño)
-
     
     def golpearPlanta(self,enemigo):
         ataque = self.daño + 2*random.randrange(-10,11)
@@ -36,21 +48,21 @@ class Personaje:
 
 
 #plantas
-LanLanzaguisantes = Personaje('Lanzaguisantes',150,15)
-Explotonuez = Personaje('Explotonuez',1,100)
-Girasol = Personaje('Girasol',100,0)
-Repetidora = Personaje('Repetidora',150,30)
-Guisantralladora = Personaje('Guisantralladora',100,40)
-Birasol = Personaje('Birasol',150,0)
-Tripitadora = Personaje('Tripitadora',150,50)
+LanLanzaguisantes = Plantas('Lanzaguisantes',150,15)
+Explotonuez = Plantas('Explotonuez',1,100)
+Girasol = Plantas('Girasol',100,0)
+Repetidora = Plantas('Repetidora',150,30)
+Guisantralladora = Plantas('Guisantralladora',100,40)
+Birasol = Plantas('Birasol',150,0)
+Tripitadora = Plantas('Tripitadora',150,50)
 
 #zombies
 
-zombieNormal = Personaje('zombieNormal',100,15)
-zombieEscudo = Personaje('zombieEscudo',150,15)
-zombieRockero = Personaje('zombieRockero',100,25)
-zombieRugbier = Personaje('zombieRugbier',100,40)
-zombieDeportista = Personaje('zombieDeportista',100,20)
+zombieNormal = Zombies('zombieNormal',100,15)
+zombieEscudo = Zombies('zombieEscudo',150,15)
+zombieRockero = Zombies('zombieRockero',100,25)
+zombieRugbier = Zombies('zombieRugbier',100,40)
+zombieDeportista = Zombies('zombieDeportista',100,20)
 
 plantas = {
     'Lanzaguisantes' : LanLanzaguisantes ,
--- juego.py
+++ juego.py
@@ -2,10 +2,6 @@ import fichas
 import random
 
 
-#def finJuego(tablero):
-
-
-
 ronda = 1
 
 
@@ -21,9 +17,10 @@ for i in fichas.tablero:
         i[0] = fichas.nombresPlantas[contPersonajes]
         contPersonajes = random.randint(0,4)
 
+end = 1
 
 
-
+posicionZombies = -1
 
 while True:
         print('\n','\t'*11,'RONDA',ronda,'\n')
@@ -36,15 +33,51 @@ while True:
                 nombrePlanta = fichas.tablero[i][0]
                 nombreZombie = fichas.tablero[i][-1]
 
-                if nombrePlanta == 'Explotonuez':
+                if nombrePlanta == 'Explotonuez' and nombreZombie != '':
                         fichas.clases.plantas[nombrePlanta].explotarZombie(fichas.clases.zombies[nombreZombie])
                 elif nombrePlanta == 'Girasol' or nombrePlanta == 'Birasol':
                         for i in range(len(fichas.tablero)):
                                 if fichas.tablero[i][0] != 'Girasol' and fichas.tablero[i][0] != 'Birasol':
                                         fichas.clases.plantas[fichas.tablero[i][0]].mejoraDaño()
-                else:
+                elif  nombreZombie != '':
                         fichas.clases.plantas[nombrePlanta].golpearZombie(fichas.clases.zombies[nombreZombie])
 
 
-        
-        break
+        for i in range(len(fichas.tablero)):
+                nombreZombie = fichas.tablero[i][-1]
+                if nombreZombie != '':
+                        if fichas.clases.zombies[nombreZombie].vida < 1:
+                                fichas.tablero[i][-1] = ''
+
+        nuevaPosicionZombie = posicionZombies-1
+
+        for i in range(len(fichas.tablero)):
+                fichas.tablero[i][nuevaPosicionZombie],fichas.tablero[i][posicionZombies] = fichas.tablero[i][posicionZombies],fichas.tablero[i][nuevaPosicionZombie]
+
+        posicionZombies-=1
+
+        print('\n'*2,'TABLERO ACTUALIZADO:')
+        for i in fichas.tablero:
+                print(i)
+
+
+        print('\n'*2,'VIDA DE PLANTAS:')
+        for i in range(len(fichas.tablero)):
+                nombrePlantas = fichas.tablero[i][0]
+                print('NOMBRE DE LA PLANTA:',fichas.clases.plantas[nombrePlantas].nombre,', DAÑO:',fichas.clases.plantas[nombrePlantas].daño,', VIDA:',fichas.clases.plantas[nombrePlantas].vida)
+
+
+        print('\n'*2,'VIDA DE ZOMBIES:')
+        for i in range(len(fichas.tablero)):
+                nombreZombies = fichas.tablero[i][posicionZombies]
+                if nombreZombies != '':
+                        print('NOMBRE DE LA ZOMBIE:',fichas.clases.zombies[nombreZombies].nombre,', DAÑO:',fichas.clases.zombies[nombreZombies].daño,', VIDA:',fichas.clases.zombies[nombreZombies].vida)
+                else:
+                        continue
+
+        end+=1
+
+        ronda+=1
+
+        if end == 4:
+                break
